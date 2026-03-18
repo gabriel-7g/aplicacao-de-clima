6e0b7e04cd412d9d37eb3c1245316bb6d5a49e5b
Gabriel <killo7425@gmail.com>
2026-03-18 09:37:38 -0300

    Adicionando funções e eventos inciais no JS

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-// Key api: ec93ff529b105c7121ed16e76cedbfda
+// Variáveis e seleção de eventos
+const apiKey =  "ec93ff529b105c7121ed16e76cedbfda";
+const apiCountryUrl = "https://flagcdn.com/16x12/"
 
+const cityInput = document.querySelector("#city-input")
+const searchBtn = document.querySelector("#search")
+
+// Funções
+const showWeatherData = (city) => {
+    console.log(city)
+}
+
+// Eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+
+})
