25c8061f8f7e28235484ac21c6cb7397832d8c1b
Gabriel <killo7425@gmail.com>
2026-03-18 11:05:10 -0300

    Finalização dos Scripts e ajustes no HTML

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ const tempElement = document.querySelector("#temperature span")
 const descElement = document.querySelector("#description")
 const weatherIconElement = document.querySelector("#weather-icon")
 const countryElement = document.querySelector("#country")
-const umidityElement = document.querySelector("#umidity span")
+const humidityElement = document.querySelector("#humidity span")
 const windElement = document.querySelector("#wind span")
+const weatherContainer = document.querySelector("#weather-data")
 
 // Funções
 const getWeatherData = async(city) =>{
@@ -20,11 +21,20 @@ const getWeatherData = async(city) =>{
     const res = await fetch(apiWeatherUrl)
     const data = await res.json()
 
-    console.log(data)
+    return data
 }
 
-const showWeatherData = (city) => {
-    getWeatherData(city)
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city)
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = `${parseInt(data.main.temp)}`;
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", `https://openweathermap.org/payload/api/media/file/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`)
+    humidityElement.innerText = `${data.main.humidity}%`
+    windElement.innerText = `${data.wind.speed}KM/H`
+    weatherContainer.classList.remove("hide")
 }
 
 // Eventos
@@ -32,4 +42,12 @@ searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
     const city = cityInput.value;
     showWeatherData(city);
+})
+
+cityInput.addEventListener("keyup", (e) =>{
+    if(e.code === "Enter"){
+        const city = e.target.value
+
+        showWeatherData(city)
+    }
 })
