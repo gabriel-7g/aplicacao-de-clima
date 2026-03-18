6d50a3b36897f82810fbad6e7aa9e23f797a16e4
Gabriel <killo7425@gmail.com>
2026-03-18 09:55:11 -0300

    Finalização da inserção dos dados da API

--- script.js
+++ script.js
@@ -5,17 +5,31 @@ const apiCountryUrl = "https://flagcdn.com/16x12/"
 const cityInput = document.querySelector("#city-input")
 const searchBtn = document.querySelector("#search")
 
+const cityElement = document.querySelector("#city")
+const tempElement = document.querySelector("#temperature span")
+const descElement = document.querySelector("#description")
+const weatherIconElement = document.querySelector("#weather-icon")
+const countryElement = document.querySelector("#country")
+const umidityElement = document.querySelector("#umidity span")
+const windElement = document.querySelector("#wind span")
+
 // Funções
+const getWeatherData = async(city) =>{
+    const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherUrl)
+    const data = await res.json()
+
+    console.log(data)
+}
+
 const showWeatherData = (city) => {
-    console.log(city)
+    getWeatherData(city)
 }
 
 // Eventos
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
-
     const city = cityInput.value;
-
     showWeatherData(city);
-
 })
