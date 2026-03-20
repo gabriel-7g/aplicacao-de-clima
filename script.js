e37a52f26be72cd22e6459704d5a11314121fd7f
Gabriel <killo7425@gmail.com>
2026-03-19 21:25:36 -0300

    Finalização do projeto adicionando mais uma API para imagens de fundo baseados na pesquisa

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Variáveis e seleção de eventos
 const apiKey =  "ec93ff529b105c7121ed16e76cedbfda";
 const apiCountryUrl = "https://flagcdn.com/16x12/"
+const cityKey = "mMX2jf7YTHH0ufeLyBhKLQJxeG9vIq0gtDd2yuNWuxY"
 
 const cityInput = document.querySelector("#city-input")
 const searchBtn = document.querySelector("#search")
+const bgImage = document.querySelector("#bg-image")
 
 const cityElement = document.querySelector("#city")
 const tempElement = document.querySelector("#temperature span")
@@ -24,16 +26,28 @@ const getWeatherData = async(city) =>{
     return data
 }
 
+const getCityPhoto = async(city) =>{
+    const apiUrl = `https://api.unsplash.com/search/photos?query=${city}&client_id=${cityKey}`
+
+    const res = await fetch(apiUrl)
+    const data = await res.json()
+
+    return data.results[0].urls.regular
+}
+
 const showWeatherData = async (city) => {
     const data = await getWeatherData(city)
+    const photoUrl = await getCityPhoto(city);
 
     cityElement.innerText = data.name;
     tempElement.innerText = `${parseInt(data.main.temp)}`;
     descElement.innerText = data.weather[0].description;
     weatherIconElement.setAttribute("src", `https://openweathermap.org/payload/api/media/file/${data.weather[0].icon}.png`);
     countryElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`)
+    bgImage.setAttribute("src", photoUrl);
     humidityElement.innerText = `${data.main.humidity}%`
     windElement.innerText = `${data.wind.speed}KM/H`
+
     weatherContainer.classList.remove("hide")
 }
 
